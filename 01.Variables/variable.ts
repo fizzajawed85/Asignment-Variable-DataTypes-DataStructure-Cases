a1082a7839e9137646c0311f660da875f5e74172
fizzajawed85 <162190155+fizzajawed85@users.noreply.github.com>
2024-03-11 22:56:17 +0500

    Add files via upload

---
+++ 01.Variables/variable.ts
@@ -0,0 +1,43 @@
+//Variable is a container which can store data.//
+// Typescript follows the same rules as JavaScript for variables declarations.//
+// Variables can be decleared using: Var,let, and const.//
+
+
+// Var //
+var a = 12 ;
+console.log(a);
+
+a = 10 ;
+console.log(a);
+
+a = a + a;
+console.log(a);
+
+
+// Let //
+let n = "Fizza";
+console.log(n);
+
+n = "Jawed";
+console.log(n);
+
+let q = true;
+console.log(q);
+
+let e = false;
+console.log(e);
+
+
+// Const //
+// variable can be decleared using const similar to var or let declarations.//
+// The const make a variable to constant where its value cannot be changed.//
+// Const variables must be declared and initialized in a single statement.//
+ 
+const gpa = 3.5;
+console.log(gpa);
+
+// Another Example //
+
+const birthYear = 2002;
+console.log(birthYear); // it will console 2002 //
+// birthYear = 2003;  => it will show an error to assign 'birthyear' because it is constant.//
