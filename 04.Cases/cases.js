1293eb3d1fe0d44265242a35ed35beeec10a685e
fizzajawed85 <162190155+fizzajawed85@users.noreply.github.com>
2024-03-11 23:01:20 +0500

    Add files via upload

---
+++ 04.Cases/cases.js
@@ -0,0 +1,30 @@
+"use strict";
+// camelCase //
+// snake_case //
+// PascalCase //
+// Example no.01 camelCase //
+let firstName = "Fizza";
+// Example no.02 snake_case //
+let last_name = "Jawed";
+// Example no.03 PascalCase //
+let FullName = "Fizza Jawed";
+console.log(firstName, last_name, FullName);
+// Example no.01 camelCase //
+let userName = true;
+// Example no.02 snake_case //
+let person_name = null;
+//Example no.03 PascalCase //
+let OtherName = 7;
+console.log(userName, person_name, OtherName);
+// Example no.01 camelCase in "string" //
+let fruitName = "Blueberry";
+console.log(fruitName);
+// Example no.02 snake_case  in "number" //
+let first_number = 0;
+console.log(first_number);
+//Example no.03 PascalCase in "Boolean" //
+let QuestionValues = true;
+console.log(QuestionValues);
+// Example no.04 "undefined" variable any Datatype //
+let anyType;
+console.log(anyType);
---
+++ 04.Cases/cases.ts
@@ -0,0 +1,45 @@
+// camelCase //
+// snake_case //
+// PascalCase //
+
+// Example no.01 camelCase //
+let firstName = "Fizza";
+// Example no.02 snake_case //
+let last_name = "Jawed";
+// Example no.03 PascalCase //
+let FullName = "Fizza Jawed";
+
+console.log(firstName, last_name ,FullName);
+
+
+// Example no.01 camelCase //
+let userName = true;
+// Example no.02 snake_case //
+let person_name = null;
+//Example no.03 PascalCase //
+let OtherName = 7;
+
+console.log(userName, person_name, OtherName);
+
+
+// Example no.01 camelCase in "string" //
+let fruitName : string = "Blueberry";
+console.log(fruitName);
+
+// Example no.02 snake_case  in "number" //
+let first_number : number = 0;
+console.log(first_number);
+
+//Example no.03 PascalCase in "Boolean" //
+let QuestionValues : boolean = true;
+console.log(QuestionValues);
+
+// Example no.04 "undefined" variable any Datatype //
+let anyType;
+console.log(anyType);
+
+
+
+
+
+
